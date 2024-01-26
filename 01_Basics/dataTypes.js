9ab92fdc24bf9e7480cd5bf0d71826e76cd5f32d
saugatpanta <124836509+saugatpanta@users.noreply.github.com>
2024-01-26 00:30:00 +0000

    Config

--- 01_Basics/dataTypes.js
+++ 01_Basics/dataTypes.js
@@ -3,7 +3,25 @@
 // alert("hello") // we are using nodejs, not browser
 
 console.log(3
-    +
-    3) // code is not readable or readable should be high 
+    + 3) // code is not readable or readable should be high 
 
     console.log("Saugat") //readable
+    
+    let name = "Purnima"
+    let age = 16
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null is standalone value
+    // undefined =>  unsigned bhako xaina value
+    // symbol => uniqueness define garna
+
+
+    // object
+
+    console.log(typeof undefined);// undefined
+    console.log(typeof null);// object
