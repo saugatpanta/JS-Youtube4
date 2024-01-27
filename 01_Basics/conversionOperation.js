841b3bbdd584e874272c86ab458a79fc8c96661b
saugatpanta <124836509+saugatpanta@users.noreply.github.com>
2024-01-27 02:26:05 +0000

    Container config

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,20 @@
+// let score = true
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" ==> 33
+// "33abd" ==> NaN
+//  true = 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let stringId = String(isLoggedIn)
+console.log(stringId)
